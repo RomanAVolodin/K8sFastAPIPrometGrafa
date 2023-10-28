edc246e136e433c0c976732a95de70ef5c504d0b
Roman Volodin <rozarioagro@gmail.com>
2023-10-28 10:42:45 +0300

    prometheus and grafana

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 
 import uvicorn
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from api.v1.posts import router
 from core.logger import LOGGING
@@ -24,6 +25,8 @@ async def startup():
 
         await create_database()
 
+
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(router, prefix='/api/v1/posts')
 
